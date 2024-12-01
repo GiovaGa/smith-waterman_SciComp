d7befc8c5afd4f84421bde3c37dbc4de5554b50c
FC0000 <126058608+FC0000@users.noreply.github.com>
2024-12-01 18:42:51 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,136 @@
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "benchmark.h"
+#include "sequence.h"
+
+int smith_waterman(int, int, const char*, const char*, int, int, int, int*);
+long smith_waterman_flops(int, int);
+
+
+void program_usage(const char* program_name)
+{
+	printf(
+		"Usage: %s [sequence1] [sequence2]\n"
+		"\twhere [sequence1] and [sequence2] are either:\n"
+		"\t\t-f <file> - read the sequence from the file <file>\n"
+		"\t\t-r <length> - generate a random sequence of length <length>\n"
+		"\t\t-p - read characters from stdin.\n",
+		program_name);
+}
+
+void bad_usage(const char* program_name)
+{
+	printf("Bad usage.\n");
+	program_usage(program_name);
+	exit(-1);
+}
+
+
+struct sequence_input
+{
+	enum class { FROM_FILE, RANDOM, FROM_STDIN } type;
+	union
+	{
+		const char* filename;
+		int random_length;
+	};
+};
+
+/*
+* Parses the command line arguments and returns the offset to the next argument.
+* Exits the program on failure.
+*/
+int parse_input(int argc, char* argv[], int argc_offset, struct sequence_input* seq_in)
+{
+	if (argc == argc_offset)
+	{
+		seq_in->type = FROM_STDIN;
+		return argc_offset;
+	}
+	if (strcmp(argv[argc_offset], "-f") == 0)
+	{
+		if (argc < argc_offset + 1) bad_usage(argv[0]);
+		seq_in->type = FROM_FILE;
+		seq_in->filename = argv[argc_offset + 1];
+		return argc_offset + 2;
+	}
+	else if (strcmp(argv[argc_offset], "-r") == 0)
+	{
+		if (argc < argc_offset + 1) bad_usage(argv[0]);
+		int len = atoi(argv[argc_offset + 1]);
+		if (len <= 0)
+		{
+			fprintf(stderr, "Invalid length\n");
+			exit(-1);
+		}
+		seq_in->type = RANDOM;
+		seq_in->random_length = len;
+		return argc_offset + 2;
+	}
+	else if (strcmp(argv[argc_offset], "-p") == 0)
+	{
+		seq_in->type = FROM_STDIN;
+		return argc_offset + 1;
+	}
+	bad_usage(argv[0]);
+}
+
+struct sequence_t get_sequence(struct sequence_input seq_in)
+{
+	switch (seq_in.type)
+	{
+		case FROM_FILE:
+			return read_sequence_from_file(seq_in.filename);
+		case RANDOM:
+			return get_random_sequence(seq_in.random_length);
+		case FROM_STDIN:
+			printf("Enter the sequence:\n");
+			return read_sequence_from_stdin();
+		default:
+			fprintf(stderr, "Invalid sequence type\n");
+			exit(-1);
+	}
+}
+
+
+int main(int argc, char* argv[])
+{
+	if (argc == 2 && strcmp(argv[1], "-h") == 0)
+	{
+		program_usage(argv[0]);
+		return 0;
+	}
+	struct sequence_t A, B;
+
+	struct sequence_input A_input, B_input;
+	int argc_offset = 1;
+
+	argc_offset = parse_input(argc, argv, argc_offset, &A_input);
+	argc_offset = parse_input(argc, argv, argc_offset, &B_input);
+
+	if (argc != argc_offset)
+		bad_usage(argv[0]);
+
+	A = get_sequence(A_input);
+	B = get_sequence(B_input);
+
+
+	printf("\n\nA (%d) =\n%s\n\nB (%d) =\n%s\n\n", A.length, A.data, B.length, B.data);
+
+	struct sw_implementation implementations[] = {
+		{ "Smith-Waterman", smith_waterman, smith_waterman_flops },
+		{ "Smith-Waterman2", smith_waterman, smith_waterman_flops },
+		{ "Smith-Waterman3", smith_waterman, smith_waterman_flops }
+	};
+
+	benchmark(implementations, sizeof(implementations) / sizeof(implementations[0]), A, B);
+
+	deallocate_sequence(A);
+	deallocate_sequence(B);
+
+	return 0;
+}
---
+++ sequence.c
@@ -0,0 +1,146 @@
+#include "sequence.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+/*
+* Allocates memory for a sequence of the specified length.
+* Adds null terminator at the end.
+* Exits the program on failure.
+*/
+static char* allocate_sequence(int length)
+{
+	if (length <= 0 || length == INT_MAX)
+	{
+		fprintf(stderr, "Error: %d is not a valid sequence length\n", length);
+		exit(-1);
+	}
+	char* data = (char*)malloc((length + 1) * sizeof(char));
+	if (!data)
+	{
+		fprintf(stderr, "Cannot allocate memory for sequence\n");
+		exit(-1);
+	}
+	data[length] = '\0';
+	return data;
+}
+
+
+void deallocate_sequence(struct sequence_t sequence)
+{
+	free((char*)sequence.data);
+}
+
+/*
+* Validates the sequence and converts it to upper case.
+* Returns true if the sequence is valid, false otherwise.
+*/
+static bool validate_sequence(char* data, int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+		char c = data[i];
+		if (c == 'a' || c == 'g' || c == 'c' || c == 't')
+		{
+			data[i] = toupper(c);
+			continue;
+		}
+		if (c != 'A' && c != 'G' && c != 'C' && c != 'T')
+			return false;
+	}
+	return true;
+}
+
+
+struct sequence_t get_random_sequence(int length)
+{
+	char* data = allocate_sequence(length);
+	for (int i = 0; i < length; i++)
+	{
+		switch (rand() % 4)
+		{
+		case 0: data[i] = 'A'; break;
+		case 1: data[i] = 'G'; break;
+		case 2: data[i] = 'C'; break;
+		case 3: data[i] = 'T'; break;
+		}
+	}
+	struct sequence_t sequence = { data, length };
+	return sequence;
+}
+
+struct sequence_t read_sequence_from_file(const char* filename)
+{
+	FILE* file = fopen(filename, "r");
+	if (!file)
+	{
+		perror("Cannot open file");
+		exit(-1);
+	}
+	fseek(file, 0, SEEK_END);
+	long file_length = ftell(file);
+	rewind(file);
+	if (file_length > INT_MAX)
+	{
+		fprintf(stderr, "File is too big\n");
+		exit(-1);
+	}
+	char* data = allocate_sequence((int)file_length);
+	int chars_read = fread(data, sizeof(char), file_length, file);
+	if (chars_read != file_length)
+	{
+		fprintf(stderr, "Cannot read the entire file\n");
+		exit(-1);
+	}
+	fclose(file);
+	if (!validate_sequence(data, (int)file_length))
+	{
+		fprintf(stderr, "Invalid sequence\n");
+		exit(-1);
+	}
+	struct sequence_t sequence = { data, (int)file_length };
+	return sequence;
+}
+
+
+struct sequence_t read_sequence_from_stdin(void)
+{
+	int buffer_size = 128;
+	char* data = NULL;
+	int length = 0;
+	while (true)
+	{
+		// here seq.data is NULL or has length characters + 1 for the null terminator
+		data = (char*)realloc(data, buffer_size * sizeof(char));
+		if (!data)
+		{
+			fprintf(stderr, "Cannot allocate memory for sequence\n");
+			exit(-1);
+		}
+		if (!fgets(data + length, buffer_size - length, stdin))
+		{
+			fprintf(stderr, "Cannot read sequence\n");
+			exit(-1);
+		}
+		int chars_read = strlen(data + length);
+		length += chars_read;
+		if (data[length - 1] == '\n' || feof(stdin))
+		{
+			length--;
+			data[length] = '\0';
+			break;
+		}
+		buffer_size *= 2;
+	}
+	if (!validate_sequence(data, length))
+	{
+		fprintf(stderr, "Invalid sequence\n");
+		exit(-1);
+	}
+	struct sequence_t sequence = { data, length };
+	return sequence;
+}
