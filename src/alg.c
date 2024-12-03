52fb70a4a0d543c0a10cc3d3e3e18566325a81f1
GiovaGa <48856010+GiovaGa@users.noreply.github.com>
2024-12-03 18:12:47 +0100

    fixup! Naive version of Smith-Waterman + proto-main - solves #1

--- src/alg.c
+++ src/alg.c
@@ -1,11 +1,6 @@
 #include<omp.h>
 
-static inline int min_between(int a, int b) {
-	if (a <= b) return a;
-	else return b;
-}
-
-static inline int max_between(int a, int b) {
+static inline int max(int a, int b) {
 	if (a >= b) return a;
 	else return b;
 }
@@ -17,15 +12,15 @@ int smith_waterman(int N, int M, const char* restrict A, const char* restrict B,
 
 	for (int i = 1; i <= N; ++i) {
 		for (int j = 1; j <= M; ++j) {
-			const int score = score_match * (A[i] == B[j]) + score_mismatch * (A[i] != B[j]);
-			H[i * (M + 1) + j] = max_between(H[i * (M + 1) + j], H[(i - 1) * (M + 1) + j - 1] + score);
+			const int score = score_match * (A[i-1] == B[j-1]) + score_mismatch * (A[i-1] != B[j-1]);
+			H[i * (M + 1) + j] = max(H[i * (M + 1) + j], H[(i - 1) * (M + 1) + j - 1] + score);
 			for (int k = 1; k < i; ++k) {
-				H[i * (M + 1) + j] = max_between(H[i * (M + 1) + j], H[(i - k) * (M + 1) + j] + score_open_gap + score_continue_gap * (k-1));
+				H[i * (M + 1) + j] = max(H[i * (M + 1) + j], H[(i - k) * (M + 1) + j] + score_open_gap + score_continue_gap * (k-1));
 			}
 			for (int k = 1; k < j; ++k) {
-				H[i * (M + 1) + j] = max_between(H[i * (M + 1) + j], H[i * (M + 1) + j - k] + score_open_gap + score_continue_gap * (k-1));
+				H[i * (M + 1) + j] = max(H[i * (M + 1) + j], H[i * (M + 1) + j - k] + score_open_gap + score_continue_gap * (k-1));
 			}
-			ans = max_between(ans, H[i * (M + 1) + j]);
+			ans = max(ans, H[i * (M + 1) + j]);
 		}
 	}
 	return ans;
